958fc19873ee87f8a4ac57b6341ad9043a91e861
Ilya Romanuk <ilya.romanuck2014@yandex.ru>
2023-06-12 16:00:03 +0300

    Added sound configs via wireplumber

---
+++ wireplumber/.config/wireplumber/main.lua.d/53-microphone-rename.lua
@@ -0,0 +1,12 @@
+rule = {
+  matches = {
+    {
+      { "node.name", "equals", "alsa_input.pci-0000_00_1f.3.analog-stereo" },
+    },
+  },
+  apply_properties = {
+    ["node.description"] = "Microphone",
+  },
+}
+
+table.insert(alsa_monitor.rules, rule)
