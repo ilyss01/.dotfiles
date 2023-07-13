2bb3ebbd44f5b214c9f05217406a0b5ec61e128c
Ilya Romanuk <ilya.romanuck2014@yandex.ru>
2023-07-13 19:53:05 +0300

    Scripts description fix

--- scripts/scripts/yt-dlp-best-audio.sh
+++ scripts/scripts/yt-dlp-best-audio.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-# Download of youtube music in best audio quality
+# Download youtube music in best audio quality
 
 # https://write.corbpie.com/downloading-youtube-videos-as-audio-with-yt-dlp/
 yt-dlp -f 'ba' --output ~/media/music/ $1 &
