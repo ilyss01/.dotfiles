20b36f3f64d79b34ea991ebf2d23d027fdfd36f3
ilia.romaniuk <ilya.romanuck2014@yandex.ru>
2024-01-02 15:20:27 +0300

    scripts update

--- scripts/scripts/post-install.sh
+++ scripts/scripts/post-install.sh
@@ -2,35 +2,43 @@
 
 pS="doas pacman -S"
 
-mkdir -p $HOME/.local/{share/{applications,gnupg},state} $HOME/.config/ .cache/
-mkdir -p $HOME/media/{.desktop,.templates,share,music} $HOME/downloads $HOME/pictures/wallpapers
+mkdir -p $HOME/{.local/{share/{applications,gnupg,wineprefixes},state},.config,.cache}
+mkdir -p $HOME/media/{.desktop,.templates,share,music} $HOME/{downloads,pictures/wallpapers}
 
 # desktop
 $pS hyprland wireplumber waybar foot \
 xdg-desktop-portal-{hyprland,wlr} xdg-{user-dirs,utils} \
-bluez{,-libs,-utils} tlp acpi acpid swaylock swayidle ttf-fira-code \
+bluez{,-libs,-utils} tlp acpi acpid swaylock swayidle \
 pipewire{,-alsa,-audio,-docs,-jack,-pulse,-session-manager} gammastep \
 wl-clipboard wlroots polkit-kde-agent udiskie hyprpaper \
-xorg-xwayland adobe-source-han-{sans-jp-fonts,serif-jp-fonts} \
-noto-fonts-cjk otf-ipafont otf-ipaexfont ttf-hanazono \
-ttf-sazanami noto-fonts ttf-firacode-nerd noto-fonts-emoji \
-gnu-free-fonts ttf-arphic-uming ttf-indic-otf otf-latin-modern \
-otf-latinmodern-math
+xorg-xwayland 
 
 # programs
-$pS lf zathura{,-pdf-poppler} bat light mpv fish helix \
+$pS light fish \
 keepassxc qutebrowser wofi chromium lazygit ytfzf taskwarrior-tui stow \
 imv telegram-desktop libreoffice-fresh texlive diskonaut reflector yt-dlp \
-fwupd{,-efi} pulsemixer chafa lynx poppler mediainfo atool \
-odt2txt texlive-lang kvantum wine-staging wine-nine wine-mono 
+fwupd{,-efi} pulsemixer texlive-lang kvantum wine-{staging,nine,mono} \
+jrnl
 
+
+# lf
+$pS lf zathura{,-pdf-poppler} bat mpv helix chafa lynx poppler \
+mediainfo atool odt2text 
+
+# fonts
+$pS ttf-fira-code adobe-source-han-{sans-jp-fonts,serif-jp-fonts} \
+noto-fonts{,-cjk,-emoji} otf-{ipafont,ipaexfont,latin-modern{,-math}} ttf-hanazono \
+ttf-sazanami ttf-firacode-nerd \
+gnu-free-fonts ttf-arphic-uming ttf-indic-otf 
 
 #virtualization
 $pS qemu-desktop virt-manager libvirt dnsmasq
 
 # programming
-$pS rustup clang gcc lldb llvm-libs ghc python rust-analyzer \
-valgrind texlab taplo python-lsp-server haskell-language-server 
+$pS rustup clang gcc lldb llvm-libs ghc rust-analyzer \
+valgrind texlab taplo haskell-language-server \
+python yapf autopep8 flake8 \
+python-{lsp-server,mccabe,pycodestyle,pydocstyle,pyflakes,pylint,rope,whatthepatch}
 
 # codecs
 $pS flac wavpack a52dec libdca libmad libmpcdec \
@@ -42,5 +50,8 @@ gst-plugins-{bad,base,good,ugly} gst-plugin-{va,pipewire} \
 intel-media-driver libva-intel-driver
 
 # daemons
-systemctl enable {fstrim.timer,acpid.service,libvirtd.service,NetworkManager.service,systemd-timesyncd.service,tlp.service}
+doas systemctl enable {fstrim.timer,acpid.service,libvirtd.service,NetworkManager.service,systemd-timesyncd.service,tlp.service}
 
+# system things
+echo "permit nopass :wheel" > /etc/doas.conf
+echo "vm.swappiness = 3" > /etc/sysctl.d/99-swappiness.conf
